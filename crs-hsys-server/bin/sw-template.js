5eabb5ff6353ae33752e42002aa219c4fc483854
CR <csernikr@gmail.com>
2018-12-31 23:13:49 +0100

    Firebase SDK v5.7.1

--- crs-hsys-server/bin/sw-template.js
+++ crs-hsys-server/bin/sw-template.js
@@ -237,8 +237,8 @@ function sendMessageToAllClients(message) {
  * If you do not serve/host your project using Firebase Hosting see
  * https://firebase.google.com/docs/web/setup
  */
-importScripts('https://www.gstatic.com/firebasejs/5.7.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.7.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.7.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.7.1/firebase-messaging.js');
 
 firebase.initializeApp({'messagingSenderId': '99934082315'});
 	  
